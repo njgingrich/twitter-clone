5b9938a273e78e4fa0a922aa8314fe6f6733aadd
nathan <njgingrich@gmail.com>
2015-11-09 01:13:43 -0500

    still not working

--- app/adapters/timeline.js
+++ app/adapters/timeline.js
@@ -4,19 +4,19 @@ import config from '../config/environment';
 export default DS.RESTAdapter.extend(DS.BuildURLMixin, {
 	headers: {
 		oauth_consumer_key: config.APP.oauth_consumer_key,
-	    oauth_token: config.APP.oauth_token,
-	    oauth_signature_method: config.APP.oauth_signature_method,
-	    oauth_version: config.APP.oauth_version
+	   	oauth_token: config.APP.oauth_token,
+	   	oauth_signature_method: config.APP.oauth_signature_method,
+	   	oauth_version: config.APP.oauth_version
 	},
 	buildUrl: function(type, id, record) {
 		return 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=njgingrich'; 
 	},
-	ajax: function(url, method, hash) {
-		hash = hash || {};
-		hash.crossDomain = true;
-		hash.xhrFields = {withCredentials: true};
-		return this._super(url, method, hash);
-	},
+	//ajax: function(url, method, hash) {
+	//	hash = hash || {};
+	//	hash.crossDomain = true;
+	//	hash.xhrFields = {withCredentials: true};
+	//	return this._super(url, method, hash);
+	//},
 	//namespace: '1.1',
 	//host: 'https://api.twitter.com/statuses',
 	findQuery: function(store, type, query) {
